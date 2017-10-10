fd4332b59f0dd461029c13cbd9079a16f2a81e64
Cassiel60 <32317762+Cassiel60@users.noreply.github.com>
2017-10-10 10:11:57 +0800

    make report by R

---
+++ R_011.r
@@ -0,0 +1,10 @@
+library(rmarkdown)
+render("C:/Users/BAIOMED07/Desktop/work/R_exercise/women.Rmd", "html_document")
+
+
+library(knitr)
+knit("C:/Users/BAIOMED07/Desktop/work/R_exercise/drugs.Rnw")
+
+
+library(knitr)
+knit2pdf("C:/Users/BAIOMED07/Desktop/work/R_exercise/drugs.Rnw")
